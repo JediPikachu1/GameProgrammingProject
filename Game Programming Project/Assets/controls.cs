12e759328ebf5174c4ce8c615f69e114dc5e8c8b
JediPikachu1 <npoched1@kent.edu>
2021-11-18 18:35:12 -0500

    Initial Commit

---
+++ Game Programming Project/Assets/controls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class controls : MonoBehaviour
+{
+    public Vector2 mousePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(mousePosition.x, mousePosition.y, 0) * Time.deltaTime );
+    }
+
+    void OnMouseMove()
+    {
+        mousePosition = Mouse.current.position.ReadValue();
+    }
+}
